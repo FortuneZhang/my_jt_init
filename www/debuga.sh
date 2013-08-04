0d0f97934bd9bcd3ad3b3253399b8c29daccdcd3
Fortune.Zhang <zhangfortune527@gmail.com>
2013-08-04 12:04:38 +0800

    fortune- auto with var

--- www/debuga.sh
+++ www/debuga.sh
@@ -1,26 +1,30 @@
 #!/usr/bin
+packagename="com.fortune.app.wp24"
+apk="p24-debug.apk"
+
 echo 'auto'
 echo 'www build...'
 gen
 mdclean
 mdbuild
 
-echo 'turn to app'
+echo 'turn to app fold'
 cd ..
 cd app
 
 echo 'uninstall app...'
 adb clean
-adb uninstall com.fortune.app.wp24
+adb uninstall $packagename
 
 echo 'app build...'
 ant debug
-adb install -r bin/p24-debug.apk
+adb install -r bin/$apk
 
-adb shell am start -n  com.fortune.app.wp24/com.fortune.app.wp24.activity.MainActivity
+adb shell am start -n  $packagename/$packagename.activity.MainActivity
 echo 'activity running'
 
 echo 'backing...'
 
 cd ..
 cd www
+echo 'please see phone'
